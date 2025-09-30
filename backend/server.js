f99775d003af16d28e3de41fc4125d51dbff4306
ouissal-yahyouni <ouissalyahyouni73@gmail.com>
2025-09-30 14:35:44 +0100

    Initial setup frontend + backend

---
+++ backend/server.js
@@ -0,0 +1,12 @@
+import express from "express";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send('Hello World!');
+});
+
+app.listen(3000, () => console.log('Server running on port 3000'));
---
+++ frontend/src/App.jsx
@@ -0,0 +1,12 @@
+function App() {
+  return (
+   <div class="d-flex justify-content-center align-items-center vh-100 bg-light">
+  <button class="btn btn-primary btn-lg shadow">
+    let's get started our poject 
+  </button>
+</div>
+
+  );
+}
+
+export default App;
